de7a4afa65b28d088f5ae3d0bdfc4dc0eef59822
ahnet-69 <syedmusicrealise@gmail.com>
2024-08-30 20:47:10 +0500

    pdx203: Initial decommonized device tree
    Based on: https://github.com/LineageOS/android_device_sony_pdx203

---
+++ BoardConfig.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sony sm8250-common
+-include device/sony/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/sony/pdx203
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := XQ-AT72,XQ-AT52,XQ-AT51,XQ-AT42,SOG01,SO-51A
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+BOARD_KERNEL_CMDLINE += buildproduct=pdx203
+
+KERNEL_DEFCONFIG := pdx203_defconfig
+
+BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+# Props
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+
+# inherit from the proprietary version
+-include vendor/sony/pdx203/BoardConfigVendor.mk
+
+# Xperia Modules
+TARGET_SUPPORTS_HIGH_REFRESH_RATE := false
---
+++ device.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sony sm8250-common
+$(call inherit-product, device/sony/sm8250-common/common.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Overlays
+PRODUCT_PACKAGES += \
+    SonyPDX203SystemUIRes \
+    SonyPDX203FrameworksRes
+
+# HDR
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/libhdr_somc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/libhdr_somc.xml
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/sony/pdx203/pdx203-vendor.mk)
