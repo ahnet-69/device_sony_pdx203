be1c060c1f3cb36ad0a413e3ba1a556aa49a9181
realahnet <syedmusicrealise@gmail.com>
2025-12-08 03:24:34 +0500

    pdx203: Restructure tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 # Props
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
 # inherit from the proprietary version
 -include vendor/sony/pdx203/BoardConfigVendor.mk
